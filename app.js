21bc173ba06552d00d840e6352bd7c5cb0ce3c26
Aun-Ali-12 <aunali29331@gmail.com>
2025-03-05 22:07:38 +0500

    assigment-2-Js

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ let visitedTimes = 21;
 alert(`You have visited ${visitedTimes} times our webpage.`);
 
 let myBirthYear = 2004;
-const poc = document.write(`My birth year is ${myBirthYear}. Data type of my decleared variable is` +" "+typeof myBirthYear + " .")
+const poc = document.write(`My birth year is ${myBirthYear}. Data type of my decleared variable is` +" "+typeof myBirthYear + " ." +"<br>" +"<br>")
 
 const visitorName = "Aun Ali";
 const productTitle = "V-neck Shirt";
 const orderedQuantity = 5;
-document.write(" " + `${visitorName} has ordered ${orderedQuantity} ${productTitle} from XYZ Clothing store.`)
+document.write(" " +`${visitorName} has ordered ${orderedQuantity} ${productTitle} from XYZ Clothing store.`)
